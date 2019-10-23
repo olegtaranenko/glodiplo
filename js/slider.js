2547cdd18d1d32b81ab60f0933fd2bbe4db60571
Oleg Taranenko <olegtaranenko@gmail.com>
2019-10-24 01:40:45 +0200

    brandnews desktop done

--- js/slider.js
+++ js/slider.js
@@ -5,24 +5,36 @@ const breakpoint_sm = 646;
 const breakpoint_xs = 520;
 const breakpoint_2xs = 396;
 const breakpoint_3xs = 320;
+let saleSlider;
 
 $(document).ready(function () {
   $(window).resize(function () {
-    let width = $(this).width();
+    let width = $(this).outerWidth();
     if (width) {
       console.log(width);
     }
   });
 
-  let width = $(window).width();
-  if (width >= breakpoint_lg) {}
-  var slider = tns({
+
+  let width = $(window).outerWidth();
+  let viewMode = 'gallery';
+  let itemsNumber = 4;
+  let autoplay = false;
+
+  if (width < breakpoint_lg) {
+    viewMode = 'carousel';
+    itemsNumber = width < breakpoint_xs ? 1 : 2;
+    autoplay = true;
+  }
+
+
+  const slideOptions = {
     container: '.my-slider',
-    mode: 'gallery'
-    // items: 3,
-    // slideBy: 'page',
-    // autoplay: true
-  });
+    mode: viewMode,
+    items: itemsNumber,
+    slideBy: 1,
+    autoplay: autoplay
+  };
 
-
+  // saleSlider = tns(slideOptions);
 });
